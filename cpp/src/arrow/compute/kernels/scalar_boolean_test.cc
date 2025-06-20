621b0846a5f4510156bb7bdf6fc71efc92f7f114
Hadrian Reppas <hadrian@hudson-trading.com>
2025-06-20 13:53:10 -0400

    Improve ChunkedOffsets test

--- cpp/src/arrow/compute/kernels/scalar_boolean_test.cc
+++ cpp/src/arrow/compute/kernels/scalar_boolean_test.cc
@@ -52,19 +52,22 @@ void CheckBooleanScalarArrayBinary(std::string func_name, Datum array) {
 }
 
 template <template <typename> class BitOp>
-void CheckChunkedOffsets(std::string func_name, std::shared_ptr<Array> trues, std::shared_ptr<Array> falses) {
+void CheckChunkedOffsets(const std::string& func_name,
+    const std::shared_ptr<Array>& a, const std::shared_ptr<Array>& b) {
   BitOp<bool> op;
 
   for (int64_t left_offset = 0; left_offset < 8; left_offset++) {
     for (int64_t right_offset = 0; right_offset < 8; right_offset++) {
       for (int64_t length = std::max(left_offset, right_offset) + 1; length <= 24; length++) {
         auto left = std::make_shared<ChunkedArray>(
-            ArrayVector{falses->Slice(0, left_offset),
-                        trues->Slice(left_offset, length - left_offset)});
-        auto right = trues->Slice(right_offset, length);
+            ArrayVector{a->Slice(0, left_offset),
+                        b->Slice(left_offset, length - left_offset)});
+        auto right = a->Slice(right_offset, length);
         ASSERT_OK_AND_ASSIGN(Datum result, CallFunction(func_name, {left, right}));
         for (int64_t i = 0; i < length; i++) {
-          BooleanScalar expected(op(i >= left_offset, true));
+          BooleanScalar l = checked_cast<BooleanScalar&>(*left->GetScalar(i).ValueOrDie());
+          BooleanScalar r = checked_cast<BooleanScalar&>(*right->GetScalar(i).ValueOrDie());
+          BooleanScalar expected(op(l.value, r.value));
           ASSERT_TRUE(result.chunked_array()->GetScalar(i).ValueOrDie()->Equals(expected));
         }
       }
@@ -180,9 +183,13 @@ TEST(TestBooleanKernel, ChunkedOffsets) {
   ASSERT_OK(builder.AppendValues(std::vector<bool>(32, false)));
   ASSERT_OK(builder.Finish(&falses));
 
-  CheckChunkedOffsets<std::bit_and>("and", trues, falses);
-  CheckChunkedOffsets<std::bit_or>("or", trues, falses);
-  CheckChunkedOffsets<std::bit_xor>("xor", trues, falses);
+  for (const auto& a : {trues, falses}) {
+    for (const auto& b : {trues, falses}) {
+      CheckChunkedOffsets<std::bit_and>("and", a, b);
+      CheckChunkedOffsets<std::bit_or>("or", a, b);
+      CheckChunkedOffsets<std::bit_xor>("xor", a, b);
+    }
+  }
 }
 
 }  // namespace compute
